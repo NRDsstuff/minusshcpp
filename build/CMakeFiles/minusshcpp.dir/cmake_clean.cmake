a1c0c7b433a3fa8000a8f1bd659c2da145b80e4c
nrdsstuff <nrdfoss@gmail.com>
2022-05-23 11:58:36 +0200

    yes

---
+++ build/CMakeFiles/minusshcpp.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/minusshcpp.dir/main.cpp.o"
+  "CMakeFiles/minusshcpp.dir/main.cpp.o.d"
+  "minusshcpp"
+  "minusshcpp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/minusshcpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/minusshcpp.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for minusshcpp.
+# This may be replaced when dependencies are built.
